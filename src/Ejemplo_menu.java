05ee7eb7b443240cb7c5d772da313fb353982c6b
Alejandra Gonzalez <ale.bonilla309@gmail.com>
2023-11-04 12:43:38 +0100

    ejemplo switch, if

---
+++ src/Ejemplo_menu.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Ejemplo_menu {
+    public static void main(String[] args) {
+        Scanner Scanner= new Scanner(System.in);
+        System.out.println("1. sumar");
+        System.out.println("2. restar");
+        System.out.println("3. dividir");
+        System.out.println("4. multiplicar");
+        System.out.println("5. Salir");
+        System.out.println("Introduce la opcion que quieres");
+        int opcion= Scanner.nextInt();
+        int operando1=0, operando2=0;
+        double resultado= 0.0;
+        if (opcion>0 && opcion<5){
+            System.out.println("introduce operando 1");
+            operando1= Scanner.nextInt();
+            System.out.println("introduce operando 2");
+            operando2= Scanner.nextInt();
+        }
+
+
+
+        switch (opcion){
+            case 1:
+                System.out.println("Vas a sumar");
+                resultado= operando1+operando2;
+
+                break;
+            case 2:
+                System.out.println("vas a restar");
+                resultado= operando1-operando2;
+
+                break;
+            case 3:
+                System.out.println("vas a dividir");
+                resultado= operando1/operando2;
+
+                break;
+            case 4:
+                System.out.println("vas a multiplicar");
+                resultado= operando1*operando2;
+
+                break;
+            case 5:
+                System.out.println("saliendo.....");
+                break;
+        }
+        System.out.println("El resultado obtendio es: "+resultado);
+
+
+    }
+}
